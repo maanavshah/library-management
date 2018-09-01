68fe1a38fa8f491c4bc26cfb3e04e633553289ac
maanav <shah.maanav.07@gmail.com>
2018-09-01 12:50:50 +0530

    Add migration books, subjects

---
+++ db/migrate/20180901065927_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.column :title, :string, :limit => 32, :null => false
+      t.column :price, :float
+      t.column :subject_id, :integer
+      t.column :description, :text
+      t.timestamps
+    end
+  end
+end
