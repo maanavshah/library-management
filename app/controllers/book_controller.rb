b39812dfe43768a5ffa7022248d22bb472b3ee3f
maanav <shah.maanav.07@gmail.com>
2018-09-02 15:43:19 +0530

    Add controller and view for books

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,58 @@
+class BookController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+    @subjects = Subject.all
+  end
+
+  # create - permit params
+  def book_params
+    params.require(:books).permit(:title, :price, :subject_id, :description)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to action: :index
+    else
+      @subjects = Subject.all
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @subjects = Subject.all
+  end
+
+  # update - permit params
+  def book_param
+    params.require(:book).permit(:title, :price, :subject_id, :description)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_param)
+      redirect_to action: :show, id: @book
+    else
+      @subjects = Subject.all
+      render :edit
+    end
+  end
+
+  def delete
+    Book.find(params[:id]).destroy
+    redirect_to action: :index
+  end
+
+  def show_subjects
+    @subject = Subject.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :book do
+  resources :book, except: :destroy do
     get :show_subjects, on: :collection
+    get :delete, on: :collection
   end
 end
